30e2a15cb7bdc51ba60fbffe98cd7e991823b5da
whitgroves <whitney.groves@gmail.com>
2023-10-01 17:31:07 -0500

    repo cleanup, added .xml file support, dependency issue resolved

---
+++ enexlib/__init__.py
@@ -0,0 +1,2 @@
+from .enexlib import read_enex, format_text
+__all__ = ['read_enex', 'format_text']
--- enexlib/enexlib.py
+++ enexlib/enexlib.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-import re
+from re import compile
 
 def read_enex(filename:str, text_only:bool=False, raw_text:bool=False, join_all:bool=False) -> list[tuple[str, str]]:
 	'''
@@ -13,7 +13,7 @@ def read_enex(filename:str, text_only:bool=False, raw_text:bool=False, join_all:
 	if filename[-5:] != '.enex' and filename[-4:] != '.xml':
 		raise ValueError('Input file must be .enex or .xml')
 	
-	with open(filename, 'r', encoding='utf-8') as file: 
+	with open(filename, 'r', encoding='utf-8') as file:
 		raw = file.read()
 
 	soup = BeautifulSoup(raw, features='xml')
@@ -40,7 +40,7 @@ def format_text(text:str, text_only:bool=False) -> str:
 		# convert divs (used for spaces) and list items
 		text = text.replace('</div><div>', newline_char)
 		text = text.replace('</li>', newline_char)
-		text = re.compile(r'<li.*?>').sub(listitem_char, text)
+		text = compile(r'<li.*?>').sub(listitem_char, text)
 		
 		# convert any special characters
 		text = text.replace('&quot;', '"')
@@ -58,11 +58,11 @@ def format_text(text:str, text_only:bool=False) -> str:
 		
 		# strip out any whitespaces or remaining unicode
 		if text_only:
-			text = re.compile(r'[\t\n\r\f\v]').sub('', text)
-			text = re.compile(r'u.{,4}').sub('', text)
+			text = compile(r'[\t\n\r\f\v]').sub('', text)
+			text = compile(r'u.{,4}').sub('', text)
 		
 		#remove all remaining special characters and tags
-		text = re.compile(r'&#\d*;?').sub(' ', str(text))
-		text = re.compile(r'<.*?>').sub(' ', text)
+		text = compile(r'&#\d*;?').sub(' ', str(text))
+		text = compile(r'<.*?>').sub(' ', text)
 	
 	return ' '.join(text.split()) # removes extra whitespaces
